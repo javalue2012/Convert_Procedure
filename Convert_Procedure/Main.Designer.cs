9bf13309e6d4b7533a128384eae3c44e842f14d0
Luong Nguyen Van <luong.nguyen@orientsoftware.com>
2020-10-21 01:03:13 +0700

    ok

---
+++ Convert_Procedure/Main.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Convert_Procedure
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtModule = new System.Windows.Forms.TextBox();
+            this.txtTypeName = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtTableName = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Module Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(473, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Type Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(75, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Table Name";
+            // 
+            // txtModule
+            // 
+            this.txtModule.Location = new System.Drawing.Point(201, 48);
+            this.txtModule.Multiline = true;
+            this.txtModule.Name = "txtModule";
+            this.txtModule.Size = new System.Drawing.Size(199, 41);
+            this.txtModule.TabIndex = 4;
+            // 
+            // txtTypeName
+            // 
+            this.txtTypeName.Location = new System.Drawing.Point(613, 48);
+            this.txtTypeName.Multiline = true;
+            this.txtTypeName.Name = "txtTypeName";
+            this.txtTypeName.Size = new System.Drawing.Size(224, 42);
+            this.txtTypeName.TabIndex = 5;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(251, 196);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(438, 212);
+            this.txtInput.TabIndex = 6;
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.Location = new System.Drawing.Point(201, 112);
+            this.txtTableName.Multiline = true;
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(199, 38);
+            this.txtTableName.TabIndex = 7;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(39, 241);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(125, 71);
+            this.btnConvert.TabIndex = 8;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(757, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 71);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtTableName);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtTypeName);
+            this.Controls.Add(this.txtModule);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtModule;
+        private System.Windows.Forms.TextBox txtTypeName;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtTableName;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Convert_Procedure/Main.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convert_Procedure
+{
+    public partial class Main : Form
+    {
+        public static string dirParameter = AppDomain.CurrentDomain.BaseDirectory + @"\OuputFile.txt";
+        private int button1WasClicked = 0;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Do you want to convert?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question); if (result == DialogResult.No)
+            {
+                return;
+            }
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    string inputValue = txtInput.Text;
+                    var tableName = txtTableName.Text;
+                    string type = txtTypeName.Text;
+                    string moduleName = txtModule.Text;
+
+                    ConvertToProcedure(inputValue, tableName, type, moduleName);
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.ToString());
+                }
+            }
+        }
+
+        private void ConvertToProcedure(string inputValue, string tableName, string type, string moduleName)
+        {
+            string procedure = $"CREATE PROCEDURE [{type}].[{moduleName}_Update_{tableName}]";
+            var listColumns = inputValue.Split(',');
+            var listParameter = new List<string>();
+            var listParameter2 = new List<string>();
+            foreach (var column in listColumns)
+            {
+                var columns = column.Replace("[", "").Replace("]", "").Replace("NULL", "").Replace("NOT", "").Replace("\t", "").Replace("\r", "").Replace("\n", "");
+                listParameter.Add(columns);
+            }
+
+            foreach (var parameter in listParameter)
+            {
+                procedure += $"\n@{parameter},";
+            }
+
+            procedure = procedure.Remove(procedure.Length - 2);
+            procedure += $" AS \n UPDATE [{type}].[{moduleName}_{tableName}] SET";
+
+            foreach (var replateDate in listParameter)
+            {
+                var rep = replateDate.Replace(",", "").Replace("[nvarchar](25)", "").Replace("[uniqueidentifier]", "").Replace("[datetime2](7)", "").Replace("[bit]", "").Replace("[nvarchar](max)", "").Replace("NULL", "").Replace("NOT", "");
+                listParameter2.Add(rep);
+            }
+
+
+            foreach (var Column in listParameter2)
+            {
+                procedure += $"\n{Column}  =  @{Column},";
+            }
+            procedure = procedure.Remove(procedure.Length - 3);
+            procedure += $"\n WHERE {listParameter2[0]} = @{listParameter2[0]} \n GO";
+
+            FileStream fParameter = new FileStream(dirParameter, FileMode.Create, FileAccess.Write);
+            StreamWriter m_WriterParameter = new StreamWriter(fParameter);
+            m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+            m_WriterParameter.Write(procedure);
+            m_WriterParameter.Flush();
+            m_WriterParameter.Close();
+        }
+    }
+}
